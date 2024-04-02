d3df8c75a91d7bc807b323c28d8057bcee3e7ab0
Vishu Kumar <1992vishukumar@gmail.com>
2024-04-02 09:25:09 +0530

    old way of calling api using useEffect

--- tanstack-query/src/App.tsx
+++ tanstack-query/src/App.tsx
@@ -1,5 +1,21 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
-  return <>app</>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/todos")
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return <>{JSON.stringify(data)}</>;
 }
 
 export default App;
